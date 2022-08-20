22b243cfcfb4adcf07059effbd9de666d9f751de
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2022-08-20 22:19:19 +0000

    Rename .intro to .intro-view

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,7 +5,7 @@ const Intro = () => {
   const { setGameState } = useContext(QuizContext)
 
   return (
-    <div className='intro'>
+    <div className='intro-view'>
       <div className='intro-container container'>
         <h1>Quizzical</h1>
         <p>Some description if needed</p>
