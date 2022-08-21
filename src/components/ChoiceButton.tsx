5d8129a56fccc4b64abad0989a51f74b9e24a365
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2022-08-21 00:19:53 +0000

    Add answer parameter type

--- src/components/ChoiceButton.tsx
+++ src/components/ChoiceButton.tsx
@@ -1,6 +1,6 @@
 import parse from 'html-react-parser'
 
-const ChoiceButton = ({ answer }) => {
+const ChoiceButton = ({ answer }: { answer: string }) => {
   return <button className='btn btn-secondary choice'>{parse(answer)}</button>
 }
 
