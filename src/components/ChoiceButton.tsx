60117cd28ab1c3f6380b7e0207e98b9cb182061d
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2022-08-21 11:26:54 +0000

    Add click handler to button

--- src/components/ChoiceButton.tsx
+++ src/components/ChoiceButton.tsx
@@ -1,7 +1,14 @@
 import parse from 'html-react-parser'
 
-const ChoiceButton = ({ answer }: { answer: string }) => {
-  return <button className='btn btn-secondary choice'>{parse(answer)}</button>
+const ChoiceButton = ({ answer, handleChoiceClick }) => {
+  return (
+    <button
+      className='btn btn-secondary choice'
+      data-answer={answer}
+      onClick={handleChoiceClick}>
+      {parse(answer)}
+    </button>
+  )
 }
 
 export default ChoiceButton
